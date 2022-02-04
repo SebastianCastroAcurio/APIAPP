52ad4ad5c51ec6a080373d42d09fcbf0fd0de6e1
DarwinLala1996 <darwinlala1996@hotmail.com>
2022-02-03 20:54:34 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# APIAPP
+
+APIAPP es una aplicación que consiste en consumir una API con React para obtener la portada, título, género, idioma, reparto, premios y nominaciones y sinopsis de películas en base a una búsqueda.
+
+Para conseguir esto se utiliza la API de OMDb que es un servicio web RESTful para obtener información de películas.
+
+- https://www.omdbapi.com/
+
+Para facilitar su implementación se hace uso de los componentes de React que son elementos autónomos que nos permiten separar la interfaz de usuario en piezas independientes, reutilizables y pensar en cada pieza de forma aislada. También se incluye en el proyecto el framework Bootstrap para el diseño.
+
+# REACT
+
+React es una biblioteca Javascript de código abierto que sirve para desarrollar aplicaciones web de una manera más ordenada y con menos código que si usamos Javascript puro o librerías como jQuery centradas en la manipulación del DOM. Permite que las vistas se asocien con los datos, de modo que si cambian los datos, también cambian las vistas.
+
+Create React App configura el ambiente de desarrollo de forma que podemos usar las últimas características de Javascript, brindando una buena experiencia de desarrollo, y optimizando nuestra aplicación para producción.
+
+`npx create-react-app my-app`
+`cd my-app`
+`npm start`
+
+# BOOTSTRAP
+
+Bootstrap es una biblioteca multiplataforma o conjunto de herramientas de código abierto para diseño de sitios y aplicaciones web.
+
+Para agregar rápidamente Bootstrap a nuestro proyecto podemos descargar el paquete o simplemente utilizamos un CDN.
+
+# APIs
+
+Las APIs permiten que nuestros productos y servicios se comuniquen con otros, sin necesidad de saber cómo están implementados. Esto simplifica el desarrollo de las aplicaciones y permite ahorrar tiempo y dinero. Brindan flexibilidad; simplifican el diseño, la administración y el uso de las aplicaciones; y ofrecen oportunidades de innovación, lo cual es ideal al momento de diseñar herramientas y productos nuevos.
+
+# Documentación de React:
+
+- https://es.reactjs.org/docs/getting-started.html
+
+# Documentación de Bootstrap:
+
+- https://getbootstrap.com/docs/5.1/getting-started/introduction/
---
+++ src/App.js
@@ -0,0 +1,90 @@
+import React, { Fragment, useState, useEffect } from "react";
+// Importo Bootstrap
+import { Container, Row, Col } from "react-bootstrap";
+// Importo Emotion
+import styled from "@emotion/styled";
+// Importo el componente Search
+import Search from "./components/Search";
+// Importo el componente Movie
+import Movie from "./components/Movie";
+// Importo Alert de Bootstrap
+import { Alert } from "react-bootstrap";
+
+import LogoMovie from "./assets/logo.png";
+
+const Logo = styled.img`
+	max-width: 250px;
+`;
+
+function App() {
+	// State de busqueda de peliculas
+	const [movie, setMovie] = useState({
+		pelicula: ""
+	});
+
+	// Extraigo con destructuring pelicula
+	const { pelicula } = movie;
+
+	// State del estado de la busqueda
+	const [search, setSearch] = useState(false);
+
+	// State para el listado
+	const [listMovie, setListMovie] = useState({});
+
+	// State para controlar los errores de busqueda
+	const [error, setError] = useState(false);
+
+	// State para controlar la visualización del listado
+	const [list, setList] = useState(false);
+
+	// useEffect para controlar cambios en el componente
+	useEffect(() => {
+		const handleApi = async () => {
+			if (search) {
+				const apiKey = "apikey=dc1f5564";
+				const url = `https://www.omdbapi.com/?t=${pelicula}&${apiKey}`;
+
+				const api = await fetch(url);
+				const movies = await api.json();
+				setSearch(false);
+				setListMovie(movies);
+				console.log(movies.Response);
+				if (movies.Response === "False") {
+					setError(true);
+				} else {
+					setError(false);
+				}
+			}
+		};
+		handleApi();
+	}, [search]);
+
+	return (
+		<Fragment>
+			<Container className="text-center" fluid>
+				<Row>
+					<Col>
+						<Logo src={LogoMovie}></Logo>
+						<h3>Buscador de películas</h3>
+					</Col>
+				</Row>
+				<Row>
+					<Col>
+						<Search
+							movie={movie}
+							setMovie={setMovie}
+							setList={setList}
+							setSearch={setSearch}
+						/>
+						{error ? (
+							<Alert variant="warning">La película que buscas no existe</Alert>
+						) : null}
+						{list ? <Movie listMovie={listMovie} /> : null}
+					</Col>
+				</Row>
+			</Container>
+		</Fragment>
+	);
+}
+
+export default App;
